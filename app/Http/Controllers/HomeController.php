96b6e25aaa53bbcb6f0efc738e48037d84bbb275
leochan-htoo <95844470+leochan-htoo@users.noreply.github.com>
2023-11-28 22:04:46 +0700

    Move Cart Data to Order Table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use App\Models\User;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
 
 class HomeController extends Controller
 {
@@ -111,5 +112,48 @@ class HomeController extends Controller
         return redirect()->back();
     }
 
+
+    //this function will use for order payment for order items
+    public function cash_order()
+    {
+        $user=Auth::user();
+
+        $userid=$user->id;
+
+        $data=cart::where('user_id','=',$userid)->get();
+
+        foreach($data as $data)
+        {
+
+         $order=new Order;
+
+         $order->name=$data->name;
+         $order->email=$data->email;
+         $order->phone=$data->phone;
+         $order->address=$data->address;
+         $order->user_id=$data->user_id;
+
+         $order->product_title=$data->product_title;
+         $order->price=$data->price;
+         $order->quantity=$data->quantity;
+         $order->image=$data->name;
+         $order->product_id=$data->product_id;
+
+         $order->payment_status='cash on delivery';
+
+         $order->delivery_status='processing';
+
+         $order->save();
+
+         $cart_id=$data->id;
+
+         $cart=cart::find($cart_id);
+
+         $cart->delete();
+
+
+        }
+        return redirect()->back()->with('message','we have Received your Order. We will connect with you soon...');
+    }
 
 }
--- resources/views/home/showcart.blade.php
+++ resources/views/home/showcart.blade.php
@@ -103,6 +103,20 @@
 
          <!-- end slider section -->
 
+         @if(session()->has('message'))
+
+            <div class="alert alert-success">
+
+                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">x
+
+                </button>
+
+                    {{ session()->get('message') }}
+
+            </div>
+
+        @endif
+
 
       <div class="">
         <table>
